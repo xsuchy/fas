1fd2f7d187f4c939034245583df48ff7830fb187
Yaakov M. Nemoy <loupgaroublond@gmail.com>
2009-02-01 17:50:40 -0500

    Adds plugin for survey
    This is just the core plugin bits. the plugin will enable us to do surveys within FAS

---
+++ plugins/fas-plugin-survey/fas_survey/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import turbogears
+from turbogears import controllers, expose, paginate, identity, redirect, widgets, validate, validators, error_handler
+from turbogears.database import session
+
+import cherrypy
+
+from genshi.template.plugin import TextTemplateEnginePlugin
+
+import fas.sidebar as sidebar
+import logging
+import fas.plugin as plugin
+
+class SurveyPlugin(controllers.Controller):
+    capabilities = ['survey_plugin']
+
+    def __init__(self):
+        '''Create a Survey Controller.'''
+        self.path = ''
+
+    @expose(template="fas_survey.templates.index")
+    def index(self):
+        value = "my Val"
+        return dict(value=value)
+
+    @classmethod
+    def initPlugin(cls, controller):
+        cls.log = logging.getLogger('plugin.survey')
+        cls.log.info('Survey plugin initializing')
+        try:
+            path, self = controller.requestpath(cls, '/survey')
+            cls.log.info('Survey plugin hooked')
+            self.path = path
+            if self.sidebarentries not in sidebar.entryfuncs:
+                sidebar.entryfuncs.append(self.sidebarentries)
+        except (plugin.BadPathException,
+            plugin.PathUnavailableException), e:
+            cls.log.info('Survey plugin hook failure: %s' % e)
+
+    def delPlugin(self, controller):
+        self.log.info('Survey plugin shutting down')
+        if self.sidebarentries in sidebar.entryfuncs:
+            sidebar.entryfuncs.remove(self.sidebarentries)
+            
+    def sidebarentries(self):
+        return [('Survey plugin', self.path)]
